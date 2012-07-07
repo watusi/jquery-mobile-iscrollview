7fe7d56d219d3323309557ef7ba9963a16998f02
Jon Tara <jtara-github-public@spamex.com>
2012-07-07 15:08:54 -0700

    demo.js, set $.mobile.maxScrollForTransition() to function returning 0. This is for new option that is part of JQM 1.1 long listview fix. (Issue #4340). Since the demo doesn't need scroll-to-top functionality for pages, disable the scroll-to-top completely. This is probably true for most projects using iScroll, so include this in the demo to show how to set the option. (jQuery Mobile starting with 1.1 scrolls pages to the top before transition. With fixed device-height pages, this is unnecessary.)

--- demo/build/javascripts/demo.js
+++ demo/build/javascripts/demo.js
@@ -29,4 +29,9 @@
 
 $(document).bind("mobileinit", function() {
   $.mobile.defaultPageTransition = "slide";
+  if ($.mobile.maxScrollForTransition) {
+    $.mobile.maxScrollForTransition = function() {
+      return 0;
+      };
+  }
 });
